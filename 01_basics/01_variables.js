c3cec44bdf99133db0c7333f1b3dd8fd6c6183e3
Hrithik123ku <95366383+Hrithik123ku@users.noreply.github.com>
2025-01-08 10:01:13 +0000

    about variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 12345
+let accountEmail = "hrithikkumar633@gmail.com"
+var accountPassword = "yuvi1432@"
+accountCity = "bengaluru"
+
+// accountId = 23456 // w cannot change constant
+accountEmail = "hrithikkumard@gmail.com"
+accountPassword = "hk@11/05/2001"
+accountCity = "Raichur"  
+console.table([accountId, accountEmail, accountPassword, accountCity])
