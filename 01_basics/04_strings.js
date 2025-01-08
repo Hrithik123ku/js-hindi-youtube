97d7867ffae505a227848b6ad7f46dbe36554e6c
Hrithik123ku <95366383+Hrithik123ku@users.noreply.github.com>
2025-01-08 12:28:07 +0000

    Strings

---
+++ 01_basics/04_strings.js
@@ -0,0 +1,13 @@
+//const name = "hrithik"
+//const repoCount  = 50;
+
+// console.log(`my name is ${name} and my count is ${repoCount}`)
+
+
+const name = new String('Hrithik')
+//console.log(name[0])
+//console.log(name.length)
+//console.log(name.__proto__)
+//console.log(name.toUpperCase())
+//console.log(name.lastIndexOf('i'))
+console.log(name.charAt(2))
